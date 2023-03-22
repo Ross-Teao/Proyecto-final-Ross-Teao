2d87b5c886f76b919f63ac700ed59d58f1f9b183
ross1 <rossteaopython@gmail.com>
2023-03-22 20:29:05 -0300

    primer commit

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', base, name="base"),
     path('inicio/', inicio, name="inicio"),
     path('logout/', exit, name="exit"),
+    path('info/', info, name="info"),
+    path('register/', register, name="register"),
+    
 ]
--- mi_app/views.py
+++ mi_app/views.py
@@ -3,20 +3,46 @@ from django.http import HttpResponse
 #para el login------------------------------------------------------------
 from django.contrib.auth import login,logout,authenticate
 from django.contrib.auth.decorators import login_required
+#para el register------------------------------------------------------------
+from .forms import CreacionUsuario
+
 
 # Create your views here.
 
+#Padre el que hereda------------------------------------------------------------
 def base(request):
 
     return render(request,"mi_app/base.html")
+
+#para el Logout------------------------------------------------------------
+def exit(request):
+
+    logout(request)
+    return redirect("base")
+
+#para el register------------------------------------------------------------
+def register(request):
+    data={
+        'form':CreacionUsuario()
+    }
+    if request.method == 'POST':
+        user_creation_form= CreacionUsuario(data=request.POST)
+        
+        if user_creation_form.is_valid():
+            user_creation_form.save()
+            user = authenticate(username= user_creation_form.cleaned_data['username'], password= user_creation_form.cleaned_data['password1'])
+            login(request,user)
+            return redirect('base')
+            
+    return render(request,"registration/register.html", data)
+
+@login_required
+def info(request):
+
+    return render(request,"mi_app/info.html")
 
 @login_required
 def inicio(request):
 
     return render(request,"mi_app/inicio.html")
 
-def exit(request):
-
-    logout(request)
-    return redirect("base")
-    
